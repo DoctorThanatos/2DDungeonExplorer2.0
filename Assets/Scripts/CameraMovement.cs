0ce37515d518f9e6b0880c97755bcf4dfe6d468c
Kyle Williamsd <williamskm44@gmail.com>
2021-07-21 21:22:10 -0400

    FIXED THE WHOLE PROJECT
    Added in the Camera Movement Script, no work done on it yet

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//4.89y -4.89y 5.07x -4.76x
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
