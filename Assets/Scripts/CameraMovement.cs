829c185b7dd90d28ee2274f940248b9e72eed7f3
Kyle Williamsd <williamskm44@gmail.com>
2021-07-23 17:33:33 -0400

    Added a Camera Movement Script
    Allows the Camera to follow the Player at a smooth rate

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,15 +6,24 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
+
+    public Transform target;
+    public float smoothing;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called based on math
+    void FixedUpdate()
     {
-        
+        if(transform.position != target.position)
+        {
+            //Set the Camera to Line up with the 2D Frame
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            //Move a percentage of the distance between 2 points
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
     }
 }
