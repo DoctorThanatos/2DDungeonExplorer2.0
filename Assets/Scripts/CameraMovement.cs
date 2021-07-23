603d09cfb40df1c8427bb195fd19c86008c21ea0
Kyle Williamsd <williamskm44@gmail.com>
2021-07-23 17:49:14 -0400

    Added a Clamp to the Camera Movement
    Camera Stops before reaching end of map, also adjusted some of the tile palette

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -9,6 +9,8 @@ public class CameraMovement : MonoBehaviour
 
     public Transform target;
     public float smoothing;
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,11 @@ public class CameraMovement : MonoBehaviour
         {
             //Set the Camera to Line up with the 2D Frame
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            
+            //Set the Camera to not go past the map boundries
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+            
             //Move a percentage of the distance between 2 points
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
