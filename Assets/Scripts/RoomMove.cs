b6f13743915f36306e90c82c5c0ab57d12c8c3b8
Kyle Williamsd <williamskm44@gmail.com>
2021-07-26 18:49:04 -0400

    Created a Title for Area Transitions
    Title Text will appear on screen in certain areas

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
-    public Vector2 cameraChange;
     //public Vector2 cameraChangemax; Used for Different Sized Rooms
     //public Vector2 cameraChangemin; User for Different Sized Rooms
+    public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraMovement cam;
+    public bool needText;
+    public string areaName;
+    public GameObject text;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +35,20 @@ public class RoomMove : MonoBehaviour
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
 
+            if(needText)
+            {
+                StartCoroutine(placenameCo());
+            }
+
         }
     }
+    private IEnumerator placenameCo()
+    {
+        text.SetActive(true);
+        placeText.text = areaName;
+        yield return new WaitForSeconds(2.5f);
+        text.SetActive(false);
+    }
 }
+
 
