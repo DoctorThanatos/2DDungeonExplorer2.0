c23510387fb95b350b166ef08397e9ccd911d5fb
Kyle Williamsd <williamskm44@gmail.com>
2021-08-05 10:09:59 -0400

    Added Knockback when hitting an Enemy
    Added a Script to allow for knockback on an enemy when hitting them

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+
+    public float thrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if(enemy != null)
+            {
+                StartCoroutine(KnockCoroutine(enemy));
+            }
+        }
+    }
+        private IEnumerator KnockCoroutine(Rigidbody2D enemy)
+        {
+            Vector2 forceDirection = enemy.transform.position - transform.position;
+            Vector2 force = forceDirection.normalized * thrust;
+
+            enemy.velocity = force;
+            yield return new WaitForSeconds(.3f);
+
+            enemy.velocity = new Vector2();
+        }
+}
